38756a69e66a9318a46cd64a1d7c463d3600151f
Jamie Pittak and Panatda Inthala <jamie@>
2018-03-20 09:04:20 -0700

    it will have create the js and hmtl files for the weather.

---
+++ weather-interface.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+  $('#weatherLocation').click(function() {
+    let city = $('#location').val();
+    $('#location').val("");
+    $.ajax({
+      url: `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=cf6ba3f84b8d27dd70d0d6241554a556
+`,
+      type: 'GET',
+      data: {
+        format: 'json'
+      },
+      success: function(response) {
+        $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
+        $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp}.`);
+      },
+      error: function() {
+        $('#errors').text("There was an error processing your request. Please try again.")
+      }
+    });
+  });
+});
